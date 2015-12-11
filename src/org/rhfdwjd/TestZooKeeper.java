c0810cb83ed06a9cbd6e39c1f4cc4832da0a0da5
krav <git@email.krav.org>
2015-12-10 21:11:28 -0500

    cleanup

--- src/org/rhfdwjd/TestZooKeeper.java
+++ src/org/rhfdwjd/TestZooKeeper.java
@@ -1,9 +1,15 @@
 package org.rhfdwjd;
 
+import com.oracle.webservices.internal.api.message.DistributedPropertySet;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.leader.LeaderSelector;
+import org.apache.curator.framework.recipes.leader.LeaderSelectorListener;
+import org.apache.curator.framework.recipes.leader.LeaderSelectorListenerAdapter;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
 import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,13 +24,29 @@ public class TestZooKeeper {
         String zookeeperConnectionString = "localhost:2181";
         CuratorFramework client = CuratorFrameworkFactory.newClient(zookeeperConnectionString, new ExponentialBackoffRetry(1000, 3));
         client.start();
-        log.info("X={}", client.getData().forPath("/test"));
+        //log.info("X={}", client.getData().forPath("/test"));
         //client.getData().watched().forPath("/test");
-        //client.create().forPath("/test", "FOOK".getBytes());
+        //client.createContainers("/test");
+        //client.setData().forPath("/test", "FOOK".getBytes());
+
+        //client.create().withMode(CreateMode.EPHEMERAL).forPath("/testEv", "FOOK".getBytes());
 
         log.info("Test");
 
-        while (true){
+        LeaderSelectorListener listener = new LeaderSelectorListenerAdapter() {
+            public void takeLeadership(CuratorFramework client) throws Exception {
+                while (true) {
+                    log.info("I'm still a big leader yea!");
+                    Thread.sleep(1000);
+                }
+            }
+        };
+
+        LeaderSelector selector = new LeaderSelector(client, "/leader", listener);
+        selector.autoRequeue();  // not required, but this is behavior that you will probably expect
+        selector.start();
+
+        while (true) {
             Thread.sleep(1);
         }
     }
